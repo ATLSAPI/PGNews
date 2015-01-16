64bdc9d0f90d8c322ec0f765892582a17d8fd641
mconi2007 <mconi2007@yahoo.com>
2015-01-16 18:43:00 +0000

    Witout Upload

---
+++ WebApplication1/Models/News.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace WebApplication1.Models
+{
+    public class News
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Body { get; set; }
+        public byte[] Image { get; set; }
+    }
+    public class NewsDBContext : DbContext
+    {
+        public DbSet<News> News { get; set; }
+    }
+}
