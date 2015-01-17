ce86851b0b9dbe5a63d5f47a342e3c5abe766229
mconi2007 <mconi2007@yahoo.com>
2015-01-17 23:52:52 +0000

    Added File upload and rich textbox control

---
+++ WebApplication1/Models/News.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Web.Mvc;
+
+namespace WebApplication1.Models
+{
+    public class News
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        [AllowHtml]
+        public string Body { get; set; }
+        public byte[] Image { get; set; }
+    }
+    public class NewsDBContext : DbContext
+    {
+        public DbSet<News> News { get; set; }
+    }
+}
